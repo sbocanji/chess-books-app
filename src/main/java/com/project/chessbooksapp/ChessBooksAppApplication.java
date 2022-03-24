e47a5437ee9b152be98eca4040cb0836ee49972e
Djina <ddjoric@griddynamics.com>
2022-03-24 09:22:55 +0100

    Project set up

---
+++ src/main/java/com/project/chessbooksapp/ChessBooksAppApplication.java
@@ -0,0 +1,13 @@
+package com.project.chessbooksapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessBooksAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessBooksAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/chessbooksapp/ChessBooksAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.chessbooksapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessBooksAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
