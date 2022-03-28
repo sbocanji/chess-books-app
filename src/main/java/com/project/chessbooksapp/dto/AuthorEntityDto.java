dbed1f2e9a712bd24ad27b285829621934831767
dsejat <dsejat@girddynamics.com>
2022-03-28 11:32:24 +0200

    Lombok added to DTO classes

--- src/main/java/com/project/chessbooksapp/dto/AuthorEntityDto.java
+++ src/main/java/com/project/chessbooksapp/dto/AuthorEntityDto.java
@@ -1,51 +1,18 @@
 package com.project.chessbooksapp.dto;
 
-public class AuthorEntityDto {
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class AuthorEntityDto {
     private String playerName;
     private boolean active;
     private boolean worldChampion;
     private String nationality;
-
-    public AuthorEntityDto() {
-    }
-
-    public AuthorEntityDto(String playerName, boolean active, boolean worldChampion, String nationality) {
-        this.playerName = playerName;
-        this.active = active;
-        this.worldChampion = worldChampion;
-        this.nationality = nationality;
-    }
-
-    public String getPlayerName() {
-        return playerName;
-    }
-
-    public void setPlayerName(String playerName) {
-        this.playerName = playerName;
-    }
-
-    public boolean isActive() {
-        return active;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
-    }
-
-    public boolean isWorldChampion() {
-        return worldChampion;
-    }
-
-    public void setWorldChampion(boolean worldChampion) {
-        this.worldChampion = worldChampion;
-    }
-
-    public String getNationality() {
-        return nationality;
-    }
-
-    public void setNationality(String nationality) {
-        this.nationality = nationality;
-    }
 }
--- src/main/java/com/project/chessbooksapp/dto/BookEntityDto.java
+++ src/main/java/com/project/chessbooksapp/dto/BookEntityDto.java
@@ -1,40 +1,16 @@
 package com.project.chessbooksapp.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class BookEntityDto {
     private String id;
     private String bookName;
     private String author;
-
-    public BookEntityDto() {
-    }
-
-    public BookEntityDto(String id, String bookName, String author) {
-        this.id = id;
-        this.bookName = bookName;
-        this.author = author;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getBookName() {
-        return bookName;
-    }
-
-    public void setBookName(String bookName) {
-        this.bookName = bookName;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
 }
