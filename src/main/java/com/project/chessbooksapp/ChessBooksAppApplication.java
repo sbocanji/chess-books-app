027e5ebcc3ca599ec1785514f68fc2f6c7a7819d
dsejat <dsejat@girddynamics.com>
2022-03-28 11:29:51 +0200

    CommandLineRunner for app testing added

--- src/main/java/com/project/chessbooksapp/ChessBooksAppApplication.java
+++ src/main/java/com/project/chessbooksapp/ChessBooksAppApplication.java
@@ -1,38 +1,12 @@
 package com.project.chessbooksapp;
-
-import com.project.chessbooksapp.dto.AuthorEntityDto;
-import com.project.chessbooksapp.dto.BookEntityDto;
-import com.project.chessbooksapp.services.readers.CSVReader;
-import com.project.chessbooksapp.services.readers.JsonReader;
-import com.project.chessbooksapp.services.readers.Reader;
-import com.project.chessbooksapp.services.readers.authors.AuthorCSVReader;
-import com.project.chessbooksapp.services.readers.books.BookCSVColumns;
-import com.project.chessbooksapp.services.readers.books.BookCSVReader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.awt.print.Book;
-import java.util.List;
 
 @SpringBootApplication
 public class ChessBooksAppApplication {
 
     public static void main(String[] args) {
-        BookCSVReader bookReader = new BookCSVReader();
-        List<BookEntityDto> books = bookReader.readFile("knjige");
-        for (BookEntityDto book : books) System.out.println("ID: " + book.getId() + ", name: " + book.getBookName() + ", author: " + book.getAuthor());
-        System.out.println("\n\n\n");
-        AuthorCSVReader authorReader = new AuthorCSVReader();
-        List<AuthorEntityDto> authors = (List<AuthorEntityDto>) authorReader.readFile("igraci");
-        for (AuthorEntityDto author : authors)
-            System.out.println("Name: " + author.getPlayerName() + ", nationality: " + author.getNationality() + ", active: " + author.isActive() + ", worldChampionship: " + author.isWorldChampion());
-        System.out.println("\n\n\n");
-        Reader<BookEntityDto> jsonReader = new JsonReader<>(BookEntityDto.class);
-        List<BookEntityDto> newBooks = jsonReader.readFile("knjigeJson.json");
-        for (BookEntityDto book : newBooks) System.out.println("ID: " + book.getId() + ", name: " + book.getBookName() + ", author: " + book.getAuthor());
-
-
-//        SpringApplication.run(ChessBooksAppApplication.class, args);
+        SpringApplication.run(ChessBooksAppApplication.class, args);
     }
 
 }
