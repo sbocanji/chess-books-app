89ea8bc857412539d404703f01c0cb58aac076fb
tbacic <tbacic@griddynamics.com>
2022-04-07 10:51:35 +0200

    list all books use case,plus @service annotattion added to impl of listallavailablebooks

--- src/main/java/com/project/chessbooksapp/book/application/service/ListAllBooksImpl.java
+++ src/main/java/com/project/chessbooksapp/book/application/service/ListAllBooksImpl.java
@@ -1,13 +1,19 @@
 package com.project.chessbooksapp.book.application.service;
 
 import com.project.chessbooksapp.book.application.port.in.ListAllBooksService;
+import com.project.chessbooksapp.book.application.port.out.BookRepository;
 import com.project.chessbooksapp.book.domain.Book;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@RequiredArgsConstructor
+@Service
 public class ListAllBooksImpl implements ListAllBooksService {
+    private final BookRepository bookRepository;
     @Override
     public List<Book> listAllBooks() {
-        return null;
+        return bookRepository.findAll();
     }
 }
--- src/main/java/com/project/chessbooksapp/book/application/service/ListAvailableBooksServiceImpl.java
+++ src/main/java/com/project/chessbooksapp/book/application/service/ListAvailableBooksServiceImpl.java
@@ -2,9 +2,10 @@ package com.project.chessbooksapp.book.application.service;
 
 import com.project.chessbooksapp.book.application.port.in.ListAvailableBooksService;
 import com.project.chessbooksapp.book.domain.Book;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class ListAvailableBooksServiceImpl implements ListAvailableBooksService {
     @Override
     public List<Book> listAvailableBooks() {
